ec2ec9169310ceb22e8d4c4a0402af480e3a47a5
w_zhanghongyi <zhy521521>
2023-08-03 19:46:38 +0800

    feature demo文档脚本

--- edu-zhy-api/src/test/java/qttTextCheck.java
+++ edu-zhy-api/src/test/java/qttTextCheck.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.derby.iapi.util.StringUtil;
 import org.junit.Test;
-import scala.Char;
 
 import java.io.*;
 import java.util.*;
